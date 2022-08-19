74c0081c0d26859afe8e42145750dadc47c2ce64
ryandefea <ryan.defea@gmail.com>
2022-08-19 13:50:30 -0500

    add pets to animal shelter context.

---
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace AnimalShelter.Models
+{
+    public class AnimalShelterContext : DbContext
+    {
+        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+{
+  builder.Entity<Pet>()
+      .HasData(
+          new Pet { PetId = 1, Name = "Bandit", Species = "Golden Retriever", Age = 4, Gender = "male" },
+          new Pet { PetId = 2, Name = "Sparky", Species = "Boston Terrier", Age = 5, Gender = "male" },
+          new Pet { PetId = 3, Name = "Bali", Species = "Balinese-Javanese", Age = 1, Gender = "female" }
+);
+}
+
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
