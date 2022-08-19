a94a52879914ac9348f244e64c5681baa6cdef7c
ryandefea <ryan.defea@gmail.com>
2022-08-19 11:46:41 -0500

    update pet model.

---
+++ AnimalShelter/Models/Pet.cs
@@ -0,0 +1,12 @@
+namespace AnimalShelter.Models
+{
+  public class Pet
+  {
+    public string PetId { get; set; }
+    public string Name { get; set; }
+    public string Species { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+
+  }
+}
