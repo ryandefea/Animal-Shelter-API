82ce9d76422927c09cec20610d363f2f5dc17bea
ryandefea <ryan.defea@gmail.com>
2022-08-19 13:25:37 -0500

    update pets controller.

---
+++ AnimalShelter/Controllers/PetsController.cs
@@ -0,0 +1,123 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using AnimalShelter.Models;
+
+namespace AnimalShelter.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class PetsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public PetsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+
+    //GET api/pets
+    [HttpGet]
+    public async Task<List<Pet>> Get(string name, string species, int age, string gender)
+    {
+      IQueryable<Pet> query = _db.Pets.AsQueryable();
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (age > 0)
+      {
+        query = query.Where(entry => entry.Age >= age);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      return await query.ToListAsync();
+    }
+
+    //POST api/destinations
+    [HttpPost]
+    public async Task<ActionResult<Pet>> Post(Pet pet)
+    {
+      _db.Pets.Add(pet);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetPet), new { id = pet.PetId }, pet);
+    }
+    // GET: api/pets/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Pet>> GetPet(int id)
+    {
+      var pet = await _db.Pets.FindAsync(id);
+
+      if (pet == null)
+      {
+        return NotFound();
+      }
+
+      return pet;
+    }
+    // PUT: api/pets/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Pet pet)
+    {
+      if (id != pet.PetId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(pet).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!PetExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+    private bool PetExists(int id)
+    {
+      return _db.Pets.Any(e => e.PetId == id);
+    }
+    // DELETE: api/Pets/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePet(int id)
+    {
+      var pet = await _db.Pets.FindAsync(id);
+      if (pet == null)
+      {
+        return NotFound();
+      }
+
+      _db.Pets.Remove(pet);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
+  }
+}
