0f1d873ae7ac7c03a6bf5da7db6a76ab733a36c5
ryandefea <ryan.defea@gmail.com>
2022-08-19 18:36:34 -0500

    add required and parameters to model.

--- AnimalShelter/Models/Pet.cs
+++ AnimalShelter/Models/Pet.cs
@@ -5,9 +5,16 @@ namespace AnimalShelter.Models
   public class Pet
   {
     public int PetId { get; set; }
+    [Required]
+    [StringLength(30)]
     public string Name { get; set; }
+    [Required]
+
     public string Species { get; set; }
+    [Required]
+    [Range(0, 25, ErrorMessage = "Age must be between 0 and 200.")]
     public int Age { get; set; }
+    [Required]
     public string Gender { get; set; }
 
   }
